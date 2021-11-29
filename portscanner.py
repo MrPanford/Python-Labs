95adb928044d20bcda56ea37e7a3e945c89ceaa5
Thomas Schneider <93461556+MrPanford@users.noreply.github.com>
2021-11-29 08:30:27 -0500

    Create portscanner.py

---
+++ portscanner.py
@@ -0,0 +1,49 @@
+#Simple Port Scanner because why not
+#MrPanford
+
+#All the imports, all the time.
+import socket
+import subprocess
+import sys
+import colorama
+from colorama import Fore, Style
+from datetime import datetime
+
+
+#clear screen
+subprocess.call('clear', shell=True)
+
+#Promp and input request
+print("Port Scanner")
+print("-" * 60)
+host = input("Please input a hostname, IP address: ")
+
+#Information
+print("-" * 60)
+print(Style.BRIGHT + "Scanning..." + Style.RESET_ALL)
+print("-" * 60)
+#Date Time Block
+time1 = datetime.now()
+
+#Scan them ports
+def scan(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((host, port))
+        print(Fore.GREEN + Style.BRIGHT + "Port open: " + str(port) + Style.RESET_ALL)
+        s.close()
+    except:
+        pass
+host
+for port in range (1,65536):
+    scan(host, port)
+
+#Date Time Block 2
+time2 = datetime.now()
+
+#scan time
+timevar = time2 - time1
+
+#information
+print('Scan Complete: ', timevar)
+
