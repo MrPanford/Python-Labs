d65106833a302c3874abff00ab9194039e10ed45
Thomas Schneider <93461556+MrPanford@users.noreply.github.com>
2021-12-16 18:44:53 -0500

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -3,6 +3,7 @@
 
 #All the imports, all the time.
 import socket
+import os
 import subprocess
 import sys
 import colorama
@@ -13,7 +14,20 @@ from datetime import datetime
 #clear screen
 subprocess.call('clear', shell=True)
 
-#Promp and input request
+#Address Resolution Protocol prompt and code
+arpin = input(Style.BRIGHT + "Would you like an ARP table? Yes or No: " + Style.RESET_ALL).lower()
+if arpin == ("yes"):
+	print("ARP table")
+	print("-" * 60)
+	arp = subprocess.run(['arp', '-a'])
+	print(arp.returncode)
+else:
+	print(Style.DIM + "Bypassing ARP Table" + Style.RESET_ALL)
+
+print("-" * 60)
+
+
+#ip or host
 print("Port Scanner")
 print("-" * 60)
 host = input("Please input a hostname, IP address: ")
@@ -25,7 +39,7 @@ print("-" * 60)
 #Date Time Block
 time1 = datetime.now()
 
-#Scan them ports
+#Port Scan block, using sockets.
 def scan(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -45,5 +59,7 @@ time2 = datetime.now()
 timevar = time2 - time1
 
 #information
-print('Scan Complete: ', timevar)
-
+print(Style.BRIGHT + 'Scan Complete: ' + Style.RESET_ALL)
+print(Style.BRIGHT + 'Scan Time:' + Style.RESET_ALL)
+print(timevar)
+print("-" * 60)
